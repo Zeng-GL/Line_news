2ad3f23400e70dee8dcb6586a1a2d5fc567bca63
Zeng-GL <73515978+Zeng-GL@users.noreply.github.com>
2023-11-18 17:51:52 -0600

    delete / setting file

--- settings.py
+++
@@ -1,9 +0,0 @@
-LINE_CHANNEL_ACCESS_TOKEN = '288Y8P1Kb/o5kGu/yDhSsZbu3PiZnghl/PzK9EXuy81GIhX9t40xxaS/ZzsFQLN6/LOHGepbP2GtWYZAIMxklmD13Du4KBNtWcJ0JbO4CBAv9U8hw13bxae7mOkBTqwGQFeQB+EQ/awQgEwDtv/ebAdB04t89/1O/w1cDnyilFU='
- 
-LINE_CHANNEL_SECRET = '1c3781afb3508433d7d2824bbc4677ff'
-
-host = 'localhost'
-port = 3306
-user = 'root'
-passwd = '54Lucas09@'
-db = 'pydb'
